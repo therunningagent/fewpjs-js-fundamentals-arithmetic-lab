f97bc285566380e63a6b26315d35df9516bf6f8d
therunningagent <luistjr@gmail.com>
2020-12-10 19:10:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+let random = Math.floor( Math.random() * 100) + 1
+
+let mod = 16 % 6 
+
+let max = Math.max( 5, 10, 15, 20)
